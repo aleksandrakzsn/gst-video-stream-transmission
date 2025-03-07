d90f5dc4f47c36805973f45d58e41c8ad518dbe6
aleksandra <faa2803@mail.ru>
2025-03-07 12:35:31 +0700

    all comms

--- udppc.py
+++ udppc.py
@@ -10,69 +10,69 @@ gi.require_version('Gst', '1.0')
 gi.require_version('GLib', '2.0')
 from gi.repository import Gst, GLib
 
-port = 24000
-stop_port = 55000
+port = 24000 #порт для приема по udp
+stop_port = 55000 #порт для чтения сигнала stop
 
 class CustomData():
     def __init__(self):
         Gst.init(None)
 
-        self.pipeline = Gst.Pipeline.new("pipeline")
-        self.udpsrc = Gst.ElementFactory.make("udpsrc", "source")
-        self.exit_flag = multiprocessing.Event()
-        self.frame_queue = multiprocessing.Queue(maxsize=2)  # Очередь для кадров
-        self.rtph264depay = Gst.ElementFactory.make("rtph264depay", "depay")
-        self.h264parse = Gst.ElementFactory.make("h264parse", "parser")
-        self.avdec_h264 = Gst.ElementFactory.make("avdec_h264", "decoder")
-        self.videoconvert = Gst.ElementFactory.make("videoconvert", "converter")
-        self.appsink = Gst.ElementFactory.make("appsink", "sink")
+        self._pipeline = Gst.Pipeline.new("pipeline")
+        self._udpsrc = Gst.ElementFactory.make("udpsrc", "source") 
+        self._exit_flag = multiprocessing.Event()
+        self._frame_queue = multiprocessing.Queue(maxsize=2)  # Очередь для кадров
+        self._rtph264depay = Gst.ElementFactory.make("rtph264depay", "depay")
+        self._h264parse = Gst.ElementFactory.make("h264parse", "parser")
+        self._avdec_h264 = Gst.ElementFactory.make("avdec_h264", "decoder")
+        self._videoconvert = Gst.ElementFactory.make("videoconvert", "converter")
+        self._appsink = Gst.ElementFactory.make("appsink", "sink")
 
-        if not all([self.pipeline, self.udpsrc, self.rtph264depay, self.h264parse, self.avdec_h264, self.videoconvert, self.appsink]):
+        if not all([self._pipeline, self._udpsrc, self._rtph264depay, self._h264parse, self._avdec_h264, self._videoconvert, self._appsink]):
             raise RuntimeError("Ошибка создания элементов")
         
-        self.udpsrc.set_property("port", port)
-        self.udpsrc.set_property("caps", Gst.Caps.from_string("application/x-rtp,media=video,encoding-name=H264"))
-        self.appsink.set_property("emit-signals", True)
-        self.appsink.set_property("sync", False)
-        self.appsink.set_property("max-buffers", 1)
-        self.appsink.connect("new-sample", self.on_frame)
+        self._udpsrc.set_property("port", port)
+        self._udpsrc.set_property("caps", Gst.Caps.from_string("application/x-rtp,media=video,encoding-name=H264"))
+        self._appsink.set_property("emit-signals", True)
+        self._appsink.set_property("sync", False)
+        self._appsink.set_property("max-buffers", 1)
+        self._appsink.connect("new-sample", self.on_frame)
 
-        self.pipeline.add(self.udpsrc)
-        self.pipeline.add(self.rtph264depay)
-        self.pipeline.add(self.h264parse)
-        self.pipeline.add(self.avdec_h264)
-        self.pipeline.add(self.videoconvert)
-        self.pipeline.add(self.appsink)
+        self._pipeline.add(self._udpsrc)
+        self._pipeline.add(self._rtph264depay)
+        self._pipeline.add(self._h264parse)
+        self._pipeline.add(self._avdec_h264)
+        self._pipeline.add(self._videoconvert)
+        self._pipeline.add(self._appsink)
 
-        self.udpsrc.link(self.rtph264depay)
-        self.rtph264depay.link(self.h264parse)
-        self.h264parse.link(self.avdec_h264)
-        self.avdec_h264.link(self.videoconvert)
-        self.videoconvert.link(self.appsink)
+        self._udpsrc.link(self._rtph264depay)
+        self._rtph264depay.link(self._h264parse)
+        self._h264parse.link(self._avdec_h264)
+        self._avdec_h264.link(self._videoconvert)
+        self._videoconvert.link(self._appsink)
 
-        self.stop_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.stop_socket.bind(("", stop_port))
+        self._stop_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._stop_socket.bind(("", stop_port))
 
         threading.Thread(target=self.listen_for_stop_signal, daemon=True).start()
 
-        self.frame_thread = None
+        self._frame_thread = None
     
-    def on_frame(self, appsink):
-        sample = appsink.emit("pull-sample") # pull-sample — сигнал GStreamer, который позволяет извлечь один кадр из потока
+    def on_frame(self, appsink): #функция обработки кадра
+        sample = appsink.emit("pull-sample")  # Извлекаем кадр из потока
         if sample:
             buffer = sample.get_buffer()
-            caps = sample.get_caps() #получение данных о кадре
+            caps = sample.get_caps()  # Получаем параметры кадра
             width = caps.get_structure(0).get_value("width")
             height = caps.get_structure(0).get_value("height")
 
-            success, mapinfo = buffer.map(Gst.MapFlags.READ) # success - успех операции, mapinfo - информация о кадре из буфера (mapinfo.data – сырые байты кадра.mapinfo.size – размер данных в буфере.)
+            success, mapinfo = buffer.map(Gst.MapFlags.READ)  # Читаем данные кадра
             if success:
-                frame = numpy.frombuffer(mapinfo.data, dtype=numpy.uint8) #преобразование байтов из буфера в массив numpy с типом данных uint8_t
+                frame = numpy.frombuffer(mapinfo.data, dtype=numpy.uint8)  # Преобразуем байты в numpy
 
                 # Обрабатываем YUV формат (I420)
-                if frame.size == width * height * 3 // 2:  #I420
+                if frame.size == width * height * 3 // 2:  # I420
                     yuv_frame = frame.reshape((height * 3 // 2, width))
-                    rgb_frame = cv2.cvtColor(yuv_frame, cv2.COLOR_YUV2BGR_I420)  # Преобразуем I420 в RGB
+                    rgb_frame = cv2.cvtColor(yuv_frame, cv2.COLOR_YUV2BGR_I420)  # Преобразуем в RGB
                 else:
                     print(f"Неизвестный формат данных, размер: {frame.size}")
                     buffer.unmap(mapinfo)
@@ -82,10 +82,10 @@ class CustomData():
 
                 # Добавляем кадр в очередь
                 try:
-                    if not self.frame_queue.full():
-                        self.frame_queue.put_nowait(rgb_frame)
+                    if not self._frame_queue.full():
+                        self._frame_queue.put_nowait(rgb_frame)
                     else:
-                        self.frame_queue.get_nowait()
+                        self._frame_queue.get_nowait()
                 except queue.Empty:
                     pass
                 except queue.Full:
@@ -93,24 +93,24 @@ class CustomData():
 
         return Gst.FlowReturn.OK
 
-    def get(self):
+    def get(self): 
         # Метод для получения кадра из очереди
         try:
-            frame = self.frame_queue.get(timeout=1)  # Тайм-аут на 1 секунду
+            frame = self._frame_queue.get(timeout=1)  # Тайм-аут на 1 секунду
             return frame
         except queue.Empty:
             return None  # Возвращаем None, если очередь пуста
         
     def start(self):
         print("Запуск")
-        self.pipeline.set_state(Gst.State.PLAYING)
+        self._pipeline.set_state(Gst.State.PLAYING)
         loop = GLib.MainLoop()
-        self.frame_thread = multiprocessing.Process(target=self._start, args=(loop, self.exit_flag))
-        self.frame_thread.start()
+        self._frame_thread = multiprocessing.Process(target=self._start, args=(loop, self._exit_flag))
+        self._frame_thread.start()
     
-    def listen_for_stop_signal(self):
+    def listen_for_stop_signal(self): #чтение сигнала остановки  
         while True:
-            data, _ = self.stop_socket.recvfrom(1024)
+            data, _ = self._stop_socket.recvfrom(1024)
             if data.decode().strip() == "stop":
                 print("\nПолучен сигнал остановки, завершаем...")
                 self.stop()
@@ -118,20 +118,19 @@ class CustomData():
     def stop(self):
         # Метод для корректного завершения работы
         print("Остановка видеопотока...")
-        self.exit_flag.set()  # Устанавливаем флаг для выхода из цикла
-        if self.pipeline:
-            self.pipeline.set_state(Gst.State.NULL)  # Останавливаем pipeline
+        self._exit_flag.set()  # Устанавливаем флаг для выхода из цикла
+        if self._pipeline:
+            self._pipeline.set_state(Gst.State.NULL)  # Останавливаем pipeline
 
-        if self.frame_thread is not None:
-            self.frame_thread.join()  # Ждем завершения фонового потока
+        if self._frame_thread is not None:
+            self._frame_thread.join()  # Ждем завершения фонового потока
     
         print("Поток остановлен")
 
     def _start(self, loop, exit_flag):
         try:
             print("Pipeline запущен.")
-            while not exit_flag:  # Основной цикл продолжается до выхода
-                # Проверяем наличие кадров и сообщений
+            while not exit_flag.is_set():  # Основной цикл продолжается до выхода
                 loop.get_context().iteration(False)
         except KeyboardInterrupt:
             self.stop()
@@ -144,7 +143,7 @@ if __name__ == "__main__":
     player.start()
 
     try:
-        while not player.exit_flag.is_set():  # Проверяем флаг остановки
+        while not player._exit_flag.is_set():  # Проверяем флаг остановки
             image = player.get()
             if image is not None and image.size > 0:  # Проверяем, что кадр не пустой
                 cv2.imshow("image", image)
